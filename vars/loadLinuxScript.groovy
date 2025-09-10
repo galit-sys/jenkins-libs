4b906bf1ad9a6e8b8fd9293e711d82bf71b46774
galit-sys <galit@oshri.co.il>
2025-09-10 14:33:58 +0300

    Update loadLinuxScript.groovy

--- vars/loadLinuxScript.groovy
+++ vars/loadLinuxScript.groovy
@@ -1,4 +1,4 @@
-def call(Map config = [:]0 {
+def call(Map config = [:]) {
   def scriptcontents = libraryResources "resources/org/foo/${config.name}"
   writeFile file: "${config.name}", text: scriptscontents
 }
