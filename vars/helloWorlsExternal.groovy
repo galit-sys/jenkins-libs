bb47b56d4f81503c5bfe77a4e349004587f743be
galit-sys <galit@oshri.co.il>
2025-09-10 14:29:03 +0300

    Update helloWorlsExternal.groovy

--- vars/helloWorlsExternal.groovy
+++ vars/helloWorlsExternal.groovy
@@ -1,3 +1,3 @@
 def call(Map config = [:]){
   loadLinuxScript(name: 'hello0-worls.bat')
-  bat "./hello-worls.bat ${config.name} ${config.dayOfWeek}"
+  bat "./hello-world.bat ${config.name} ${config.dayOfWeek}"
